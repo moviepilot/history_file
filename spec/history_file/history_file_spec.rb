7919935c17883a2a16a2f5f01816a8dce15bece1
Jannis Hermanns <jannis@gmail.com>
2012-11-20 14:57:27 +0100

    Updates comments and prepares for other prefix separator

--- spec/history_file/history_file_spec.rb
+++ spec/history_file/history_file_spec.rb
@@ -5,33 +5,39 @@ describe HistoryFile::FileDelegator do
 
   let(:fd){ HistoryFile::FileDelegator.new("some_prefix") }
 
-  after(:all) do
-    File.unlink("some_prefix-rspec_tmp_test.txt") rescue nil
-  end
-
-  it "delegates generic methods to File directly" do
-    File.should_receive(:split).with("/some/path")
-    fd.split("/some/path")
-  end
-
-  it "delegates bulk methods with all parameters prefixed" do
-    offset = Date.parse("1983-04-03")
-    File.should_receive(:unlink).with("./1983.04.03-file1", "./1983.04.03-file2")
-    HistoryFile[offset].unlink("file1", "file2")
-  end
-
-  it "instanciating new File instances works as expected" do
-    f = fd.new("rspec_tmp_test.txt", "w")
-    f.write("works")
-    f.close
-    File.read("some_prefix-rspec_tmp_test.txt").should == "works"
-  end
-
-  it "writing into a file using a block works as expected" do
-    fd.open("rspec_tmp_test.txt", "w") do |file|
-      file.write("works")
+  context "prefixing the filename" do
+    after(:all) do
+      File.unlink("some_prefix-rspec_tmp_test.txt") rescue nil
     end
-    File.read("some_prefix-rspec_tmp_test.txt").should == "works"
+
+    it "delegates generic methods to File directly" do
+      File.should_receive(:split).with("/some/path")
+      fd.split("/some/path")
+    end
+
+    it "delegates bulk methods with all parameters prefixed" do
+      offset = Date.parse("1983-04-03")
+      File.should_receive(:unlink).with("./1983.04.03-file1", "./1983.04.03-file2")
+      HistoryFile[offset].unlink("file1", "file2")
+    end
+
+    it "instanciating new File instances works as expected" do
+      f = fd.new("rspec_tmp_test.txt", "w")
+      f.write("works")
+      f.close
+      File.read("some_prefix-rspec_tmp_test.txt").should == "works"
+    end
+
+    it "writing into a file using a block works as expected" do
+      fd.open("rspec_tmp_test.txt", "w") do |file|
+        file.write("works")
+      end
+      File.read("some_prefix-rspec_tmp_test.txt").should == "works"
+    end
+  end
+
+  context "prefixing with sub directories" do
+
   end
 end
 
